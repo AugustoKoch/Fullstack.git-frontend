378da21230ca1fe2b62550f6e7f4f8f693585442
Andriel <167723839+andrielcf@users.noreply.github.com>
2024-08-23 20:19:26 -0300

    fix(App): Resolvendo erro de componentes, substituindo Swtich por Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LivroList from './components/LivroList';
 import UsuarioList from './components/UsuarioList';
 import './App.css';
@@ -8,10 +8,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/livros" component={LivroList} />
-          <Route path="/usuarios" component={UsuarioList} />
-        </Switch>
+        <Routes>
+          <Route path="/livros" element={<LivroList />} />
+          <Route path="/usuarios" element={<UsuarioList />} />
+        </Routes>
       </div>
     </Router>
   );
